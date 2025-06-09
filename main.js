75f259117211f621d735e85638a8b932a75f433d
sonia-droid <sonia.jimenez@ocvts.org>
2025-06-09 12:52:21 +0000

    sounds added

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ async function play3DSound(url, x = 0, y = 0, z = -1) {
 document.getElementById("listenBtn").addEventListener("click", () => {
   if (audioCtx.state === 'suspended') audioCtx.resume();
 
-  play3DSound('sounds/footsteps.mp3', -1, 0, -2); // left
-  setTimeout(() => play3DSound('sounds/whisper.mp3', 1, 0, -1), 2000); // right
-  setTimeout(() => play3DSound('sounds/door-creak.mp3', 0, 0, -3), 4000); // center
+  play3DSound('sounds/272506__sturmankin__lino_11a_sneakers_walk.wav', -1, 0, -2); // left
+  setTimeout(() => play3DSound('sounds/763800__shangusburger__creaethr_crowd-of-demonic-whispers_shanevincent_gsc24_msdec-mkh435-spirit.wav', 1, 0, -1), 2000); // right
+  setTimeout(() => play3DSound('sounds/71183__alexir__door-creak.mp3', 0, 0, -3), 4000); // center
 });
